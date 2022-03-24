0d530477ea03830319bbee10a90fa7b7497c8d90
mariosantu <mario.santucci95@gmail.com>
2022-03-24 13:53:02 +0100

    added style

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+// Data
 let add = document.getElementById('increment');
 let decrement = document.getElementById('decrement');
+let reset = document.getElementById('reset');
 let num = document.getElementById('number');
 
+// Events
 add.addEventListener('click', function() {
     num.innerHTML++;
 });
@@ -9,3 +12,8 @@ add.addEventListener('click', function() {
 decrement.addEventListener('click', function() {
     num.innerHTML--;
 });
+
+reset.addEventListener('click', function() {
+    num.innerHTML = 0;
+});
+
